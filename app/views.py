8f5233386129137a88d17e0bf099b8680dc6f195
Bryan <Bwong727@gmail.com>
2016-07-20 23:56:39 -0400

    Completed Chapter four database

--- app/views.py
+++ app/views.py
@@ -2,37 +2,39 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname': 'Miguel'} #fake user
-	posts = [ # fake array of posts
-		{
-			'author': {'nickname':'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'nickname': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		},
-		{
-			'author': {'nickname': 'Mike'},
-			'body': 'I will go to the store today.'
-		}
-	]
-	return render_template('index.html', 
-							title='Home', 
-							user=user,
-							posts=posts)
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Mike'},
+            'body': 'I will go to the store today.'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for OpenID="%s", remember_me=%s' %
-				(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html', 
-							title="Sign In", 
-							form=form,
-							providers=app.config['OPENID_PROVIDERS'])
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title="Sign In",
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
